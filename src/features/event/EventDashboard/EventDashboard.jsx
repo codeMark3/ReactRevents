3b7222c62eb6d081dd4b51b26742143945124764
Marc St-Pierre <mstpfirebase@gmail.com>
2018-07-28 13:35:45 -0400

    End of Section 5

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Grid, Button } from 'semantic-ui-react'
 import EventList from '../EventList/EventList'
 import EventForm from '../EventForm/EventForm'
+import cuid from 'cuid'
 
 const eventsDashbord = [
   {
@@ -71,7 +72,17 @@ class EventDashboard extends Component {
       isOpen: false
     })
   };
-  
+
+  handleCreateEvent = (newEvent) => {
+    newEvent.id = cuid();
+    newEvent.hostPhotoURL = '/assets/user.png';
+    const updateEvents = [...this.state.events, newEvent];
+    this.setState({
+      events: updateEvents,
+      isOpen: false
+    })  
+  }
+
   render() {
     return <Grid>
         <Grid.Column width={10}>
@@ -80,7 +91,7 @@ class EventDashboard extends Component {
         <Grid.Column width={6}>
           <Button onClick={this.handleFormOpen} positive content="Create Event" />
           {this.state.isOpen &&
-          <EventForm handleCancel={this.handleCancel} />}
+          <EventForm createEvent={this.handleCreateEvent} handleCancel={this.handleCancel} />}
         </Grid.Column>
       </Grid>;
   }
